5ab77b84634bfc3465d651d0111434e7795bade5
jafars <jafarshod99@gmail.com>
2023-04-13 10:46:23 +0700

    API HTTP Request, SWR Own Hooks, Fetcher Function, SSR with SWR

--- pages/api/users.js
+++ pages/api/users.js
@@ -5,14 +5,36 @@ import main from "../database/connection";
 import UserModel from "../database/schema";
 
 export default function get_Users(req, res) {
-  main().catch((error) => console.error(error));
+  res.status(200).json([{ id: 1, name: "Sherlock" },{id:2,name:'Holmes'}]);
+  // main().catch((error) => console.error(error));
 
-  const create = new UserModel({
-    name: "Thomas Slebew"
-  });
-  create.save().then(() => {
-    res.status(200).json(create);
-  });
+  // const { method } = req;
+
+  // switch (method) {
+  //   case "GET":
+  //     res.status(200).json({ method: "GET", endpoint: "Users" });
+  //     break;
+  //   case "POST":
+  //     res.status(200).json({ method: "POST", endpoint: "Users" });
+  //     break;
+  //   default:
+  //     res.setHeader("Allow", ["GET", "POST"]);
+  //     res.status(405).end(`Method ${method} Not Allowed`);
+  //     break;
+  // }
+
+  // ---------------------------------------------------------
+
+  // save data ke mongoDB
+
+  // const create = new UserModel({
+  //   name: "Thomas Slebew"
+  // });
+  // create.save().then(() => {
+  //   res.status(200).json(create);
+  // });
+
+  // --------------------------------------------------------
 
   // res.status(200).json([
   //   { id: 1, name: "John Cena" },
--- pages/posts/user.js
+++ pages/posts/user.js
@@ -1,32 +1,67 @@
-import useSWR from 'swr';
+import useSWR, {SWRConfig} from "swr";
 
-const fetcher = (...args) => fetch(...args).then((res) => res.json());
+const fetcher = async (url) => {
+  const res = await fetch(url);
 
+  if (!res.ok) {
+    const error = new Error("An error occured while fetching the data");
+    error.info = await res.json();
+    error.status = res.status;
+    throw error;
+  }
 
-export default function Users({ users }) {
-  const {data, error} = useSWR('http://localhost:3000/api/users', fetcher)
-  
+  return res.json()
+};
+
+export default function Users({ fallback }) {
+  return (
+    <SWRConfig value={{fallback}}>
+      {getData()}
+    </SWRConfig>
+  )
+}
+
+export async function getStaticProps() {
+  const res = await fetch("http://localhost:3000/api/users");
+  const users = await res.json();
+
+  return {
+    props: {
+      fallback : {
+        '/api/users' : users
+      }
+    },
+  };
+}
+
+function getData() {
+  const { data, error } = useSWR("http://localhost:3000/api/users", fetcher);
+
   if (error) return <div>Error fetching data</div>;
   if (!data) return <div>Loading...</div>;
-  
+
   return (
     <article>
-      {users.map((u) => (
+      {data.map((u) => (
         <div>
           <h1>{u.name}</h1>
         </div>
       ))}
     </article>
   );
+
+  // const { data, error } = useSWR("http://localhost:3000/api/users", fetcher, {
+  //   onErrorRetry: (error,key,config,revalidate,{retryCount})=> {
+  //     if(error.status === 404) return
+  //     if(key === '/api/user') return
+  //     if(retryCount >= 10) return
+  //     setTimeout(() => revalidate({retryCount}), 5000)
+  //   }
+  // });
+  // return {
+  //   user: data,
+  //   isLoading: !error && !data,
+  //   isError: error,
+  // };
 }
 
-export async function getStaticProps(){
-  const res = await fetch('http://localhost:3000/api/users')
-  const users = await res.json()
-
-  return{
-    props:{
-      users
-    }
-  }
-}
